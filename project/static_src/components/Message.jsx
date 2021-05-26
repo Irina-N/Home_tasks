6b39f40cc98195116fd3bd681357e050729d22e5
Irina Narožniaja <i.narozniaja@gmail.com>
2021-05-26 11:33:34 +0300

    home task 3

--- project/static_src/components/Message.jsx
+++ project/static_src/components/Message.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 
 export default class Message extends React.Component {
-    static PropTypes = {
-        text: PropTypes.string.isRequired,
-        author: PropTypes.string,
+    static propTypes = {
+        text: propTypes.string.isRequired,
+        sender: propTypes.string,
     };
 
     render() {
-        return <div style={{ backgroundColor: "#CCC", width: "350px", minHeight: "30px", borderRadius: "8px", padding: "3px", marginBottom: "10px" }}>
-            <p className="message_text" style={{ fontSize: "16px" }}>{this.props.text}</p>
-            <p className="message_author" style={{ textAlign: "right", color: "tomato", fontSize: "12px" }}>{this.props.author}</p>
+        return <div className="message" style={{ alignSelf: this.props.sender == 'me' ? 'flex-end' : 'flex-start' }}>
+            <p className="message_text">{this.props.text}</p>
+            <p className="message_sender">{this.props.sender}</p>
         </div >
     }
 }
--- project/static_src/index.jsx
+++ project/static_src/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/MessageField.jsx';
+import Layout from './components/Layout.jsx';
+/* import MuiThemeProvider from '@material-ui/core/styles'; */
 
 ReactDOM.render(
-    <App />,
+
+    < Layout />,
+
     document.getElementById('root'),
 );
